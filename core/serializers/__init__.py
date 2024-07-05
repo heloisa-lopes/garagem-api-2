ddce34b4619b12a94bfd3489ca1b60cc0d4e36d7
heloisa-lopes <heloisalopesalbano@gmail.com>
2024-07-05 15:09:33 -0300

    acabei

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,2 +1,7 @@
 from .user import UserSerializer
 from .acessorio import AcessorioSerializer
+from .categoria import CategoriaSerializer
+from .cor import CorSerializer
+from .marca import MarcaSerializer
+
+
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,2 +1,7 @@
 from .user import UserViewSet
 from .acessorio import AcessorioViewSet
+from .categoria import CategoriaViewSet
+from .cor import CorViewSet
+from .marca import MarcaViewSet
+
+
